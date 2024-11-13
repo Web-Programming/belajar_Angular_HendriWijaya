2182b032c572f6a756f9578489b7216034497955
Hendriwijayaaa <160854650+Hendriwijayaaa@users.noreply.github.com>
2024-11-13 20:36:28 +0700

    1

--- belajar_Angular/src/app/home/home.component.ts
+++ belajar_Angular/src/app/home/home.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import {CommonModule} from '@angular/common';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,HousingLocationComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  export interface HousingLocation{
+    id: number,
+    name:string,
+    city:string,
+    state:string,
+    photo:string,
+    availableUnits:number,
+    wifi:boolean,
+    laundry: boolean
 }
+
+

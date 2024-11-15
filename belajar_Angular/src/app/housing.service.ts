af4a160091d2f658ca6020940b3ebcb6f74eaf3f
Hendri <hendriwijaya_2226240165@mhs.mdp.ac.id>
2024-11-15 18:31:47 +0700

    1

--- belajar_Angular/src/app/housing.service.ts
+++ belajar_Angular/src/app/housing.service.ts
@@ -6,14 +6,32 @@ import { HousingLocation } from './housing-location';
 })
 export class HousingService {
   protected housingLocationlist:HousingLocation[]=[
-
-  ]
+    {
+      id: 0,
+      name: "name 1",
+      city: "city 1",
+      state: "state 1",
+      photo: "https://tjh.com/wp-content/uploads/2023/06/TJH_HERO_TJH-HOME@2x-1-1536x1021.webp",
+      availableUnits: 10,
+      wifi: true,
+      laundry: true
+    },
+    {
+      id: 1,
+      name: "name 2",
+      city: "city 2",
+      state: "state 2",
+      photo: "https://images.squarespace-cdn.com/content/v1/65a8583b3f2bb32732bff587/63ff3986-3c95-4422-bdaa-6a373b71140d/Custom-Luxury-Home-Dallas.jpg",
+      availableUnits: 12,
+      wifi: true,
+      laundry: true
+  
   constructor() { }
 
   getAllHousingLocations() : HousingLocation[]{
     return this.housingLocationlist;
   }
-  getHousingLocationById(id: Number): HousingLocation{
+  getHousingLocationById(id: Number): HousingLocation | undefined{
     return this.housingLocationlist.find(housingLocation => housingLocation.id == id)
   }
 }

07cf62f7daeffcc0a85dd7baeecec71501d7f8d6
Hendri <hendriwijaya_2226240165@mhs.mdp.ac.id>
2024-11-15 17:47:23 +0700

    .

--- belajar_Angular/src/app/app.routes.ts
+++ belajar_Angular/src/app/app.routes.ts
@@ -3,6 +3,12 @@ import { HomeComponent } from './home/home.component';
 export const routes: Routes = [
     { 
         path: '', 
-        component: HomeComponent
+        component: HomeComponent,
+        title: 'Home Page'
+    },
+    {
+    path: 'details', 
+    component: HomeComponent,
+    title: 'Details Page'
     }
 ];

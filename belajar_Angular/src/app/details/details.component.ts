f0ecaa647b488a69d5b08a4291ff6b33f10006f8
Hendriwijayaaa <160854650+Hendriwijayaaa@users.noreply.github.com>
2024-11-27 11:36:11 +0700

    .

--- belajar_Angular/src/app/details/details.component.ts
+++ belajar_Angular/src/app/details/details.component.ts
@@ -1,18 +1,16 @@
-import { Component, Inject, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housing-location';
-import { FormGroup, FormControl, ReactiveFormsModule  } from '@angular/forms';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [ReactiveFormsModule],
   template: `
-     <article >
-      <img class="listing-photo" [src]="housingLocation?.photo"
-        alt="Exterior photo of {{housingLocation?.name}}"/>
+    <article>
+      <img class="listing-photo" [src]="housingLocation?.photo" alt="Exterior photo of {{housingLocation?.name}}" />
       <section class="listing-description">
         <h2 class="listing-heading">{{housingLocation?.name}}</h2>
         <p class="listing-location">{{housingLocation?.city}}, {{housingLocation?.state}}</p>
@@ -20,54 +18,58 @@ import { FormGroup, FormControl, ReactiveFormsModule  } from '@angular/forms';
       <section class="listing-features">
         <h2 class="section-heading">About this housing location</h2>
         <ul>
-          <li>Units available: {{housingLocation?.availableUnits}}</li>
+          <li>Units available: {{housingLocation?.availableUnit}}</li>
           <li>Does this location have wifi: {{housingLocation?.wifi}}</li>
           <li>Does this location have laundry: {{housingLocation?.laundry}}</li>
         </ul>
       </section>
       <section class="listing-apply">
-        <h1 class="section-heading">Apply to live here</h1>
+        <h1 class="section-heading">Register to live here</h1>
         <form [formGroup]="applyForm" (submit)="submitApplyForm()">
           <label for="first-name">First Name</label>
-          <input type="text" id="first-name" formControlName="firstName" placeholder="Input first name">
+          <input type="text" id="first-name" formControlName="firstname" placeholder="Input first name">
           <label for="last-name">Last Name</label>
-          <input type="text" id="last-name" formControlName="lastName" placeholder="Input last name">
-          
+          <input type="text" id="last-name" formControlName="lastname" placeholder="Input last name">
           <label for="email">Email</label>
-          <input type="email" id="email" formControlName="email" placeholder="Input email">
+          <input type="text" id="email" formControlName="email" placeholder="Input email">
           <button type="submit" class="primary">Apply</button>
         </form>
       </section>
     </article>
   `,
-  styleUrl: './details.component.css'
+  styleUrls: ['./details.component.css']
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
   route: ActivatedRoute = inject(ActivatedRoute);
-  housingLocationId = 0;
   housingService: HousingService = inject(HousingService);
-  housingLocation: HousingLocation | undefined
-  applyForm: FormGroup = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    email: new FormControl('')
-  })
 
-  constructor(){
-    this.housingLocationId = Number(this.route.snapshot.params['id']);
+  housingLocationId = 0;
+  housingLocation: HousingLocation | undefined;
+
+  applyForm: FormGroup = new FormGroup({
+    firstname: new FormControl(''),
+    lastname: new FormControl(''),
+    email: new FormControl('')
+  });
+
+  ngOnInit(): void {
+    // Menangkap parameter id dari URL
+    this.housingLocationId = Number(this.route.snapshot.paramMap.get('id'));
+
+    // Memanggil service untuk mengambil data housing berdasarkan id
     this.housingService.getHousingLocationById(this.housingLocationId)
       .then(location => {
         this.housingLocation = location;
       })
-    console.table(this.housingLocation)
+      .catch(error => {
+        console.error('Error fetching housing location:', error);
+      });
   }
 
-  submitApplyForm(){
-    //panggil API simpan data registarsi via service
-    this.housingService.submitApplication(
-      this.applyForm.value.firstName ?? '',
-      this.applyForm.value.lastName ?? '',
-      this.applyForm.value.email ?? '',
-    )
+  submitApplyForm(): void {
+    const { firstname, lastname, email } = this.applyForm.value;
+
+    this.housingService.submitApplication(firstname, lastname, email);
+    alert(`Application submitted for ${firstname} ${lastname}`);
   }
 }

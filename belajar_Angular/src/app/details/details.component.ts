22c20438a2191e782e6aab7e133361e540e40f25
Hendriwijayaaa <160854650+Hendriwijayaaa@users.noreply.github.com>
2024-12-04 19:20:57 +0700

    .

--- belajar_Angular/src/app/details/details.component.ts
+++ belajar_Angular/src/app/details/details.component.ts
@@ -3,103 +3,71 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housing-location';
-import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl, ReactiveFormsModule  } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
   template: `
-    <article>
-      <img
-        class="listing-photo"
-        [src]="housingLocation?.photo"
-        alt="Exterior photo of {{ housingLocation?.name }}"
-      />
+     <article >
+      <img class="listing-photo" [src]="housingLocation?.photo"
+        alt="Exterior photo of {{housingLocation?.name}}"/>
       <section class="listing-description">
-        <h2 class="listing-heading">{{ housingLocation?.name }}</h2>
-        <p class="listing-location">
-          {{ housingLocation?.city }}, {{ housingLocation?.state }}
-        </p>
+        <h2 class="listing-heading">{{housingLocation?.name}}</h2>
+        <p class="listing-location">{{housingLocation?.city}}, {{housingLocation?.state}}</p>
       </section>
       <section class="listing-features">
         <h2 class="section-heading">About this housing location</h2>
         <ul>
-          <li>Units available: {{ housingLocation?.avaliableUnits }}</li>
-          <li>Does this location have wifi: {{ housingLocation?.wifi }}</li>
-          <li>
-            Does this location have laundry: {{ housingLocation?.laundry }}
-          </li>
+          <li>Units available: {{housingLocation?.availableUnits}}</li>
+          <li>Does this location have wifi: {{housingLocation?.wifi}}</li>
+          <li>Does this location have laundry: {{housingLocation?.laundry}}</li>
         </ul>
       </section>
       <section class="listing-apply">
         <h1 class="section-heading">Apply to live here</h1>
         <form [formGroup]="applyForm" (submit)="submitApplyForm()">
           <label for="first-name">First Name</label>
-          <input
-            type="text"
-            id="first-name"
-            formControlName="firstName"
-            placeholder="Input first name"
-          />
+          <input type="text" id="first-name" formControlName="firstName" placeholder="Input first name">
           <label for="last-name">Last Name</label>
-          <input
-            type="text"
-            id="last-name"
-            formControlName="lastName"
-            placeholder="Input last name"
-          />
+          <input type="text" id="last-name" formControlName="lastName" placeholder="Input last name">
+          
           <label for="email">Email</label>
-          <input
-            type="text"
-            id="email"
-            formControlName="email"
-            placeholder="Input Email"
-          />
+          <input type="email" id="email" formControlName="email" placeholder="Input email">
           <button type="submit" class="primary">Apply</button>
         </form>
       </section>
     </article>
   `,
-  styleUrl: './details.component.css',
+  styleUrl: './details.component.css'
 })
-
 export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
   housingLocationId = 0;
   housingService: HousingService = inject(HousingService);
-  housingLocation: HousingLocation | undefined;
+  housingLocation: HousingLocation | undefined
   applyForm: FormGroup = new FormGroup({
     firstName: new FormControl(''),
     lastName: new FormControl(''),
-    email: new FormControl(''),
-  });
+    email: new FormControl('')
+  })
 
-  constructor() {
+  constructor(){
     this.housingLocationId = Number(this.route.snapshot.params['id']);
-    this.housingService
-      .getHousingLocationById(this.housingLocationId)
-      .then((location) => {
+    this.housingService.getHousingLocationById(this.housingLocationId)
+      .then(location => {
         this.housingLocation = location;
-      });
-    console.table(this.housingLocation);
+      })
+    console.table(this.housingLocation)
   }
 
-  submitApplyForm() {
-    alert(
-      'Hallo : ' +
-        'First Name: ' +
-        this.applyForm.value.firstName +
-        ' Last Name: ' +
-        this.applyForm.value.lastName
-    );
-
+  submitApplyForm(){
+    //panggil API simpan data registarsi via service
     this.housingService.submitApplication(
       this.applyForm.value.firstName ?? '',
       this.applyForm.value.lastName ?? '',
-      this.applyForm.value.email ?? ''
-    );
+      this.applyForm.value.email ?? '',
+    )
   }
-  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
-  //panggil API simpan data registrasi via service
 }
--- belajar_Angular/src/app/home/home.component.ts
+++ belajar_Angular/src/app/home/home.component.ts
@@ -1,27 +1,23 @@
 import { Component, inject } from '@angular/core';
-// import untuk menggabungkan home dan housing
-
-import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { CommonModule } from '@angular/common';
 import { HousingLocation } from '../housing-location';
 import { HousingService } from '../housing.service';
-
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule, HousingLocationComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css',
+  styleUrl: './home.component.css'
 })
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
-
-  constructor() {
-    // this.housingLocationList =
+  
+  constructor(){
     this.housingService.getAllHousingLocations()
-      .then((listData: HousingLocation[]) => {
-        this.housingLocationList = listData;
-      });
+    .then((listData: HousingLocation[]) =>{
+      this.housingLocationList = listData;
+    });
   }
 }

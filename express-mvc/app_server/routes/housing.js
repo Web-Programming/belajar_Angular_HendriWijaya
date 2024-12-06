f975daf41e3ebbd64223cdf889ad64b483b35951
Hendri <hendriwijaya_2226240165@mhs.mdp.ac.id>
2024-12-06 16:57:33 +0700

    .
    .

--- express-mvc/app_server/routes/housing.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const housingController = require("../controllers/controllerHousing");
-// Fungsi dan Rute Index Kita Gunakan Untuk Memanggil Semua Data Dalam Database MongoDB
-router.get("/", housingController.Index);
-router.get("/:id", housingController.Show);
-
-module.exports = router;
